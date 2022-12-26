535079c33102a21aba4f2759cc598cf5d1a6bd55
Juan Nebbia <juan.nebbia@gmail.com>
2022-12-26 02:35:31 -0300

    Create all endpoints for products and carts

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const apiRoutes = require('./routers/app.routers')
+
+const PORT = 8080
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended:true }))
+app.use('/api', apiRoutes)
+
+
+app.listen(PORT, ()=>{
+    console.log('Listening on port => ', PORT)
+})
